3f1112b5886b83cad06e9d64fafb05b289a00dac
xXCoolinXx <collin.t.francel@proton.me>
2023-07-13 22:56:03 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,26 @@
+# VectorLMS-Skip
+
+## Usage:
+
+This script requires you to install the Greasemonkey browser extension. Follow these steps to use it:
+
+* Paste the contents of `skip.js` into a new Greasemonkey userscript
+* Navigate to the module you want the script to complete
+* Let the script take you to the first two tasks
+    * These two tasks require manual intervention
+    * The first task is a series of questions. Quickly fill in random answers to get past it
+    * The second task is a video with a survey, questions and/or YouTube video embedded inside. Follow these steps
+        * Temporarily disable the GreaseMonkey extension. 
+        * Start the video
+        * Surveys can be skipped through without answering anything
+        * Questions must be clicked through fully
+        * YouTube videos you can skip the time to the end and it will still allow you to continue
+        * Re-enable GreaseMonkey before the video ends
+        * You will be automatically taken back to the home page and the script will begin working again
+* Now, the script will automatically go through and watch each video in the background
+* Occasionally, it may stop and the video will be gone from the page
+    * When this happens, REFRESH the page (do NOT go back to the home page as this will break the script)
+    * The video will need to be started manually as it has embedded questions you must answer
+    * Follow the instructions for task 2 to complete this video
+* If all goes well, you will finally reach the assessment screen. These questions must be answered manually. 
+* If you are not able to score the required 80%, you should go back and watch the videos, and question your life choices.
---
+++ skip.js
@@ -0,0 +1,81 @@
+// ==UserScript==
+// @name     VectorLMS-Skip
+// @version  1
+// @grant    GM.getValue 
+// @grant    GM.setValue
+// @match 	 https://uastudents-al.safecolleges.com/*
+// @grant unsafeWindow
+// @run-at document-end
+// ==/UserScript==
+
+
+
+//Get all tasks available on the web page
+const items = Array.from(document.getElementsByClassName('TOC_item'));
+
+//Wait a period of time
+function asyncWaitSeconds(seconds) {
+    return new Promise((resolve, reject) => {
+      setTimeout(() => {
+        resolve();
+      }, seconds * 1000);
+    });
+}
+
+//Function to convert string into seconds
+function getTimeInSeconds(str) {
+    const time = str.replace(/\s/g, '').slice(5); //Remove all spaces and newlines, then take only the characters after / (format is 0:00/time)
+
+    // Split into minutes and seconds
+    const [mins, secs] = time.split(':');  
+  
+    // Convert to seconds and return
+    return Number(mins) * 60 + Number(secs);
+}
+
+//set and get the local variable corresponding to the task number in the item array
+function setTN(num){
+  window.localStorage.setItem('task_num',num);
+}
+
+function getTN(){
+ return window.localStorage.getItem('task_num'); 
+}
+
+//If the task number doesn't already exist, create it
+if(getTN() == null) { 
+	setTN(0);
+}
+
+let task_number = Number(getTN());
+
+//Wait 5 seconds to make sure that the video loads fully and the script doesn't break
+asyncWaitSeconds(5).then(() => {
+	if(items.length > 0 && items[task_number]) {
+        //Follow the link of the current task
+        console.log("Going to task");
+        let task_link = items[task_number].href;
+        task_number += 1;
+      	setTN(task_number);
+
+        window.location.href = task_link;
+    } else if (items.length == 0) {
+        //"Play" the video so that credit is added for watching it
+        console.log("Completing task");
+        unsafeWindow.ss_player_tracking.start_tracking();
+      
+        let time_unprocessed = document.getElementsByClassName('slip_playback_num')[0].textContent;
+        let time = getTimeInSeconds(time_unprocessed);
+				console.log(time);
+      
+        asyncWaitSeconds(time).then(() => {
+            unsafeWindow.ss_player_tracking.finish_tracking();
+
+            //unsafeWindow.return_to_launcher();
+        });
+    } else {
+        //Script finished, set the number back to 0 so you can do it again for another VectorLMS module
+   	    task_number = 0; 
+        setTN(task_number);
+    }
+});
